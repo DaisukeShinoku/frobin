7a29a8bd8e73b010f494b7ea8057c84d9e81c5be
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2024-04-28 16:57:21 +0900

    マイグレーションファイル修正

--- db/migrate/20240428065831_create_leagues.rb
+++ db/migrate/20240428065831_create_leagues.rb
@@ -1,6 +1,6 @@
 class CreateLeagues < ActiveRecord::Migration[7.1]
   def change
-    create_table :leagues, id: :uuid  do |t|
+    create_table :leagues, id: :uuid do |t|
       t.string :name, null: false, limit: 50
       t.timestamps
     end
